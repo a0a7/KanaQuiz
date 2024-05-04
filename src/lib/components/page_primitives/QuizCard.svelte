4e2c0b8a25d5de6175f5657503f0f922d0b08ecf
levtus <i@alexander.systems>
2024-05-04 10:31:43 -0500

    Add basic quiz card

---
+++ src/lib/components/page_primitives/QuizCard.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+    import * as Card from "$lib/components/ui/card/index.js";
+    import * as Select from "$lib/components/ui/select/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { Label } from "$lib/components/ui/label/index.js";
+</script>
+
+<Card.Root class="w-[350px]">
+    <Card.Header>
+        <Card.Title></Card.Title>
+        <Card.Description>Deploy your new project in one-click.</Card.Description>
+    </Card.Header>
+    <Card.Content>
+        <form>
+        <div class="grid w-full items-center gap-4">
+            <div class="flex flex-col space-y-1.5">
+            <Label for="name">Name</Label>
+            <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div class="flex flex-col space-y-1.5">
+            <Label for="framework">Framework</Label>
+            </div>
+        </div>
+        </form>
+    </Card.Content>
+    <Card.Footer class="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+    </Card.Footer>
+</Card.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
 <script lang="ts">
+    import QuizCard from "$lib/components/page_primitives/QuizCard.svelte";
 </script>
 
+<div class="w-full h-full flex justify-center align-center pt-10">
+    <QuizCard />
+</div>
