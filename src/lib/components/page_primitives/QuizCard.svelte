602b6afd4b0dc3db5a87b38b50c9b56da88e7239
levtus <i@alexander.systems>
2024-05-05 20:35:18 -0500

    Add types, basic quiz input card

--- src/lib/components/page_primitives/QuizCard.svelte
+++ src/lib/components/page_primitives/QuizCard.svelte
@@ -4,19 +4,22 @@
     import * as Select from "$lib/components/ui/select/index.js";
     import { Input } from "$lib/components/ui/input/index.js";
     import { Label } from "$lib/components/ui/label/index.js";
+
+    export let input;
+    export let allowProgression = false;
+    export let currentCharacter: string;
 </script>
 
 <Card.Root class="w-[350px]">
-    <Card.Header>
-        <Card.Title></Card.Title>
-        <Card.Description>Deploy your new project in one-click.</Card.Description>
-    </Card.Header>
     <Card.Content>
+        <div class="grid w-full items-center justify-center py-5">
+            <h2 class="text-5xl">{currentCharacter}</h2>
+       </div>
         <form>
         <div class="grid w-full items-center gap-4">
             <div class="flex flex-col space-y-1.5">
-            <Label for="name">Name</Label>
-            <Input id="name" placeholder="Name of your project" />
+            <Label for="romaji">Answer</Label>
+            <Input id="romaji" placeholder="Romaji" bind:this={input} />
             </div>
             <div class="flex flex-col space-y-1.5">
             <Label for="framework">Framework</Label>
@@ -24,8 +27,11 @@
         </div>
         </form>
     </Card.Content>
-    <Card.Footer class="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
+    <Card.Footer class="flex justify-between items-center justify-center">
+        {#if allowProgression}
+            <Button class="w-32" on:click={() => {allowProgression = !allowProgression}}>Next</Button>
+        {:else}
+            <Button class="w-32"  variant="secondary" on:click={() => {allowProgression = !allowProgression}}>Check Answer</Button>
+        {/if}
     </Card.Footer>
 </Card.Root>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,23 @@
-// place files you want to import through the `$lib` alias in this folder.
+export type KanaData = {
+    [key: string]: {
+        [key: string]: {
+        Seion?: {
+            Katakana: string;
+            Hiragana: string;
+            Romaji: string;
+        };
+        Dakuon?: {
+            Katakana: string;
+            Hiragana: string;
+            Romaji: string;
+        };
+        Handakuon?: {
+            Katakana: string;
+            Hiragana: string;
+            Romaji: string;
+        };
+        };
+    };
+};
+
+export type KanaSettings = {seion: boolean, dakuon: boolean, handakuon : boolean}
