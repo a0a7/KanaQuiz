524f208201498a9fa6cc8491b84a20b85cf76dd6
levtus <i@alexander.systems>
2024-05-07 08:28:23 -0500

    Handle random selection of kana from pool that matches selected settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,17 @@
 
     let data: KanaData;
     
-    let seion = [];
-    let dakuon = [];
-    let handakuon = [];
+    let seion: { hiragana: string; katakana: string; romaji: string; }[] = [];
+    let dakuon: { hiragana: string; katakana: string; romaji: string; }[] = [];
+    let handakuon: { hiragana: string; katakana: string; romaji: string; }[] = [];
+    let shownCharacters: string[] = localStorage.getItem('shownCharacters') ? JSON.parse(localStorage.getItem('shownCharacters') ?? '') : [];
+    let kanaLeft: number;
+    let allowProgression = false;
+    let inputElement: HTMLInputElement;
+    let currentCharacter: string = pickCharacter();
+    let useSVG = true;
+    let hiraganaSettings: KanaSettings;
+    let katakanaSettings: KanaSettings;
 
     onMount(async () => {
         const response = await fetch('/kana.json');
@@ -30,13 +38,35 @@
             }
         }
         console.log(seion, dakuon, handakuon);
+        currentCharacter = pickCharacter();
     });
-    let allowProgression = false;
-    let inputElement: HTMLInputElement;
-    let currentCharacter: string = "ああ";
-    let useSVG = true;
-    let hiraganaSettings: KanaSettings;
-    let katakanaSettings: KanaSettings;
+
+    function pickCharacter() {
+        let hiraganaPool: string[] = [];
+        let katakanaPool: string[] = [];
+
+        if (hiraganaSettings.seion) { hiraganaPool = hiraganaPool.concat(seion.map((el: { hiragana: any; }) => el.hiragana)); }
+        if (hiraganaSettings.dakuon) { hiraganaPool = hiraganaPool.concat(dakuon.map((el: { hiragana: any; }) => el.hiragana)); }
+        if (hiraganaSettings.handakuon) { hiraganaPool = hiraganaPool.concat(handakuon.map((el: { hiragana: any; }) => el.hiragana)); }
+
+        if (!hiraganaSettings.monographs) { hiraganaPool = hiraganaPool.filter(el => el.length !== 1); }
+        if (!hiraganaSettings.digraphs) { hiraganaPool = hiraganaPool.filter(el => el.length !== 2); }
+
+        if (katakanaSettings.seion) { katakanaPool = katakanaPool.concat(seion.map((el: { katakana: any; }) => el.katakana)); }
+        if (katakanaSettings.dakuon) { katakanaPool = katakanaPool.concat(dakuon.map((el: { katakana: any; }) => el.katakana)); }
+        if (katakanaSettings.handakuon) { katakanaPool = katakanaPool.concat(handakuon.map((el: { katakana: any; }) => el.katakana)); }
+
+        if (!katakanaSettings.monographs) { katakanaPool = katakanaPool.filter(el => el.length !== 1); }
+        if (!katakanaSettings.digraphs) { katakanaPool = katakanaPool.filter(el => el.length !== 2); }
+
+        const pool: string[] = hiraganaPool.concat(katakanaPool).filter(el => !el).filter(el => !shownCharacters.includes(el));
+        console.log(pool)
+        currentCharacter = pool[Math.floor(Math.random() * pool.length)];
+        kanaLeft = pool.length - 1;
+        shownCharacters.push(currentCharacter)
+        localStorage.setItem('shownCharacters', JSON.stringify(shownCharacters));
+        return currentCharacter;
+    }
 </script>
 
 <ScrollArea class='h-[100vh] w-full'>
@@ -47,10 +77,10 @@
         <div class="w-full min-h-[100vh] grid grid-cols-1 md:grid-cols-5 gap-2 content-center items-center pt-16 md:pt-2">
             <div class="md:col-span-1"></div>
             <div class="flex items-center justify-center md:col-span-2">
-                <QuizCard input={inputElement} bind:allowProgression bind:currentCharacter {useSVG}/>
+                <QuizCard input={inputElement} bind:allowProgression bind:currentCharacter {useSVG} {pickCharacter}/>
             </div>
             <div class="flex justify-center md:col-span-2">
-                <Settings bind:useSVG {hiraganaSettings} {katakanaSettings}/>
+                <Settings bind:useSVG bind:hiraganaSettings bind:katakanaSettings/>
             </div>
         </div>
         <div class="grow pt-4">
