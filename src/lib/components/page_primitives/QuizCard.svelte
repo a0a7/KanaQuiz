5ac2cd72afc4db6c8c5e339c81a5a09b2e692330
levtus <i@alexander.systems>
2024-05-22 09:16:46 -0500

    Checking styles

--- src/lib/components/page_primitives/QuizCard.svelte
+++ src/lib/components/page_primitives/QuizCard.svelte
@@ -5,15 +5,20 @@
     import * as Select from "$lib/components/ui/select/index.js";
     import { Input } from "$lib/components/ui/input/index.js";
     import { Label } from "$lib/components/ui/label/index.js";
+
     export let input: string;
     export let allowProgression = false;
     export let currentCharacter: string;
     export let useSVG: boolean;
     export let shownCharacters: number;
     export let kanaLeft: number;
+    export let wonCharactersCount: number;
+    export let lostCharactersCount: number;
     export let pickCharacter: (replace: boolean) => string;
     export let checkAnswer: () => KanaResult;
 
+    let feedbackElement: HTMLDivElement;
+    let correctAnswer: boolean | undefined = undefined;
     let animationKey = 0;
     function restartAnimation() {
         animationKey++;
@@ -22,27 +27,43 @@
     function answerCheck() {
         const answerDetails = checkAnswer();
         if (answerDetails.input && answerDetails.romaji && answerDetails.input === answerDetails.romaji) {
-            console.log('correct')
+            correctAnswer = true;
+            feedbackElement.innerHTML = `
+                ${answerDetails.romaji}
+                <svg class="fill-green-500 w-[15px] h-[15px] inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+            `
         } else {
-            console.log ('incorrect')
+            correctAnswer = false;
+            feedbackElement.innerHTML = `
+                <svg class="fill-red-500 w-[15px] h-[15px] inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+                Incorrect!<br>Correct Answer: <i>${answerDetails.romaji}</i>
+            `
         }
         allowProgression = !allowProgression;
     }
     function progress() {
+        input = '';
+        feedbackElement.innerHTML = '';
         currentCharacter = pickCharacter(true);
         allowProgression = !allowProgression
+        console.log(wonCharactersCount, lostCharactersCount)
     }
 </script>
 
 <Card.Root class="w-[350px]">
-    <Card.Content>
+    <Card.Content class="relative">
         {#if kanaLeft}
-            <div class="absolute"><p>{@html kanaLeft} kana left in pool</p></div>
+            <div class="absolute"><p>{kanaLeft} remaining</p></div>
         {/if}
-        <div class="grid w-full items-center justify-center py-5 w-64 h-32">
+        {#if wonCharactersCount && shownCharacters}
+            <div class="absolute right-6">{wonCharactersCount} / {shownCharacters}</div>
+        {:else}
+            <div class="absolute right-6">0 / 0</div>
+        {/if}
+        <div class="grid w-full items-center justify-center mt-2 pt-8 pb-2">
             {#if currentCharacter && currentCharacter !== ' '}
-                {#if useSVG}
-                    <div class="flex flex-row relative w-64 h-32">
+                {#if useSVG && !(currentCharacter.length > 1)}
+                    <div class="flex flex-row relative h-32">
                         {#each currentCharacter.split('') as char}
                             <img src={`svg/${char.codePointAt(0)}.svg?${animationKey}`} alt={char} class="w-full h-full dark:invert" />
                         {/each}
@@ -51,7 +72,7 @@
                         </button>
                     </div>
                 {:else}
-                    <h2 class="text-[8rem] leading-none">{currentCharacter}</h2>
+                    <h2 class="text-[8rem] leading-none tracking-tighter">{currentCharacter}</h2>
                 {/if}
             {/if}
        </div>
@@ -61,17 +82,28 @@
                 <Label for="romaji">Answer</Label>
                 <Input id="romaji" placeholder="Romaji" bind:value={input} />
             </div>
-            <div class="flex flex-col space-y-1.5">
-                <Label for="framework">Framework</Label>
-            </div>
         </div>
         </form>
+        <div class="w-full pt-4 flex justify-between items-center justify-center">
+            {#if allowProgression}
+                {#if correctAnswer === true}
+                    <Button class="w-32 mx-auto outline outline-3 outline-green-500 outline-offset-1" on:click={progress}>Next</Button>
+                {:else if correctAnswer === false}
+                    <Button class="w-32 mx-auto outline outline-3 outline-red-500 outline-offset-1" on:click={progress}>Next</Button>
+                {:else}
+                    <Button class="w-32 mx-auto" on:click={progress}>Next</Button>
+                {/if}
+            {:else}
+                {#if input && input !== ""}
+                    <Button class="w-32 mx-auto"  variant="secondary" on:click={answerCheck}>Check Answer</Button>
+                {:else}
+                    <Button class="w-32 mx-auto"  variant="secondary" disabled>Check Answer</Button>
+                {/if}
+            {/if}
+        </div>
     </Card.Content>
     <Card.Footer class="flex justify-between items-center justify-center">
-        {#if allowProgression}
-            <Button class="w-32" on:click={progress}>Next</Button>
-        {:else}
-            <Button class="w-32"  variant="secondary" on:click={answerCheck}>Check Answer</Button>
-        {/if}
+        <div bind:this={feedbackElement} class="leading-none text-center h-[33.1px]">
+        </div>
     </Card.Footer>
 </Card.Root>
