6928d2f8345664a369e3396c0a0941164f50ee7c
levtus <i@alexander.systems>
2024-05-22 13:19:20 -0500

    Add dark mode toggle

--- src/lib/components/page_primitives/Settings.svelte
+++ src/lib/components/page_primitives/Settings.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { onMount } from 'svelte';
+    import { mode, toggleMode } from 'mode-watcher';
     import { Button } from "$lib/components/ui/button/index.js";
     import * as Card from "$lib/components/ui/card/index.js";
     import * as Select from "$lib/components/ui/select/index.js";
@@ -47,11 +48,63 @@
     $: if (typeof window !== 'undefined' && katakanaSettings) {
         localStorage.setItem('katakanaSettings', JSON.stringify(katakanaSettings));
     };
-
 </script>
 
-<Card.Root class="w-[350px]">
-    <Card.Header>
+<Card.Root class="w-[350px] relative">
+    <div class="absolute right-4 top-4">
+        <Button 
+        class="py-[6px] px-[10px] bg-card hover:bg-muted border"
+        on:click={() => {
+            toggleMode();
+        }}
+    >
+        {#if $mode === 'dark'}
+            <svg
+                class="svg-icon feather feather-moon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="black"
+                stroke-width="3"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                ><circle cx="12" cy="12" r="5" /><line x1="12" y1="1" x2="12" y2="3" /><line
+                    x1="12"
+                    y1="21"
+                    x2="12"
+                    y2="23"
+                /><line x1="4.22" y1="4.22" x2="5.64" y2="5.64" /><line
+                    x1="18.36"
+                    y1="18.36"
+                    x2="19.78"
+                    y2="19.78"
+                /><line x1="1" y1="12" x2="3" y2="12" /><line x1="21" y1="12" x2="23" y2="12" /><line
+                    x1="4.22"
+                    y1="19.78"
+                    x2="5.64"
+                    y2="18.36"
+                /><line x1="18.36" y1="5.64" x2="19.78" y2="4.22" /></svg
+            >
+        {:else}
+            <svg
+                class="svg-icon feather feather-moon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="black"
+                stroke-width="3"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                ><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" /></svg
+            >
+        {/if}
+    </Button>
+    </div>
+    <Card.Header >
         <Card.Title>Settings</Card.Title>
     </Card.Header>
     <Card.Content>
